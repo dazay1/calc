97bc7064f442dac8358d415a60a7ed477861b3e6
Yusuf Najmiddinov <najmiddinovyusuf3@gmail.com>
2023-06-26 13:32:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let h = document.getElementById('inp1')
+let h2 = document.getElementById('inp2')
+let a = document.getElementById('h1')
+let b = document
+function asd() {
+  console.log(h)
+  console.log(h2)
+  console.log(a)
+  a.innerText = parseInt(h.value)  + parseInt(h2.value)
+}
+function asdminus() {
+  console.log(h)
+  console.log(h2)
+  console.log(a)
+  a.innerText = h.value  - h2.value
+}
+function asdk() {
+  console.log(h)
+  console.log(h2)
+  console.log(a)
+  a.innerText = h.value  * h2.value
+}
+function asdb() {
+  console.log(h)
+  console.log(h2)
+  console.log(a)
+  a.innerText = h.value  / h2.value
+}
